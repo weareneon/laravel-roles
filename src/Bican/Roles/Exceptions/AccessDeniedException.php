4237a950b960f09328c0a84ce31e011083930d70
Dan Lawson <danlawson@gmail.com>
2015-11-16 15:53:23 +0000

    Inital FWM rebase

---
+++ src/Bican/Roles/Exceptions/AccessDeniedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FWM\Roles\Exceptions;
+
+use Exception;
+
+class AccessDeniedException extends Exception
+{
+    //
+}
---
+++ src/config/roles.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Package Connection
+    |--------------------------------------------------------------------------
+    |
+    | You can set a different database connection for this package. It will set
+    | new connection for models Role and Permission. When this option is null,
+    | it will connect to the main database, which is set up in database.php
+    |
+    */
+
+    'connection' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Separator
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the slug separator. This is very important in matter
+    | of magic method __call() and also a `Slugable` trait. The default value
+    | is a dot.
+    |
+    */
+
+    'separator' => '.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    |
+    | If you want, you can replace default models from this package by models
+    | you created. Have a look at `FWM\Roles\Models\Role` model and
+    | `FWM\Roles\Models\Permission` model.
+    |
+    */
+
+    'models' => [
+        'user' => FWM\AdminAuth\Entities\Administrator::class,
+        'role' => FWM\Roles\Models\Role::class,
+        'permission' => FWM\Roles\Models\Permission::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Roles, Permissions and Allowed "Pretend"
+    |--------------------------------------------------------------------------
+    |
+    | You can pretend or simulate package behavior no matter what is in your
+    | database. It is really useful when you are testing you application.
+    | Set up what will methods is(), can() and allowed() return.
+    |
+    */
+
+    'pretend' => [
+
+        'enabled' => false,
+
+        'options' => [
+            'is' => true,
+            'can' => true,
+            'allowed' => true,
+        ],
+
+    ],
+
+];
